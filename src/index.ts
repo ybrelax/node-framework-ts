7adfdfe5653f7f5fe732b458b07ae5b20ac09b27
yaobo <1195022367@qq.com>
2020-07-25 00:23:53 +0800

    提交

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import * as Koa from 'koa';
-import * as Router from 'koa-router';
+import * as Router from '@koa/router';
 import * as bodyParser from 'koa-bodyparser';
+import {logger} from "./logger";
 import AppRoutes from './routes';
 
 const app = new Koa();
 const router = new Router();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 //路由
 AppRoutes.forEach(route => router[route.method](route.path, route.action));
 
+
+// register middleware
+app.use(logger());
 app.use(bodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
